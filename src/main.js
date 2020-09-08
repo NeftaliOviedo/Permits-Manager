dc735bfddc1ea156315d3b97dc0863845bfe085c
NefOv <benjaminoviedo52@gmail.com>
2020-09-07 22:28:06 -0400

    fixing errors of publish

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueToastr2 from 'vue-toastr-2'
-import 'vue-toastr-2/dist/vue-toastr-2.min.css'
  
-window.toastr = require('toastr')
-createApp(App).use(router,VueToastr2).mount('#app')
+createApp(App).use(router).mount('#app')
 
